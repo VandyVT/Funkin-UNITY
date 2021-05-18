457dd62a5ab91d75f0d19e1628eec1571b0922e9
Vanderley5974X <jvictorjohnv@gmail.com>
2021-05-18 15:16:00 -0300

    Initial Commit

---
+++ Assets/Scripts/Main Menu/Global.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    [SerializeField] private bool StoryMode;
+    [SerializeField] private bool Freeplay;
+
+    public GameObject IntroMusic;
+    public GameObject MenuMusic;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        IntroMusic = GameObject.Find("IntroMusic");
+        if(IntroMusic)
+        {
+            MenuMusic.GetComponent<AudioSource>().Stop();
+        }
+
+        else
+        {
+            MenuMusic.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Main Menu/Intro/MenuStart.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour
+{
+    [SerializeField] private bool IntroMode;
+    [SerializeField] private bool IsLoading;
+    [SerializeField] private bool AllowStart;
+    [SerializeField] private AudioSource MenuMusic;
+    [SerializeField] private AudioSource UIStartSound;
+    [SerializeField] private Animator IntroCanvas;
+    [SerializeField] private Animator GradientCanvas;
+    [SerializeField] private Animator EnterButton;
+
+    public KeyCode kCode;
+    private bool AllowEnter;
+    [SerializeField] private float normalizedTime;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void MusicBeginner()
+    {
+        if(IntroMode == true)
+        {
+            MenuMusic.Play();
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(kCode) && IsLoading == true)
+        {
+            IntroCanvas.Play("StarterWait", 0, normalizedTime);
+        }
+
+        if (Input.GetKeyDown(kCode) && AllowStart == true && AllowEnter == false)
+        {
+            AllowEnter = true;
+            UIStartSound.Play();
+            EnterButton.Play("ButtonStart");
+            Invoke("GradientPlayer", 1.8f);
+            Invoke("MenuLoader", 2.5f);
+        }
+    }
+
+    void GradientPlayer()
+    {
+        GradientCanvas.Play("GradientMove");
+    }
+
+    void MenuLoader()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
