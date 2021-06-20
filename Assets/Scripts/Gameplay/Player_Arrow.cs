4ffa9d0494b23a45e1b74bef2d65857f1b0f5d07
Vanderley5974X <jvictorjohnv@gmail.com>
2021-06-20 01:31:28 -0300

    Arrow Animations
    Gameplay Arrow animations Added

---
+++ Assets/Scripts/Gameplay/Player_Arrow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Arrow : MonoBehaviour
+{
+    [SerializeField] private Animator PlayerArrows;
+
+    void Update()
+    {
+        //Left Arrow Animation
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PlayerArrows.Play("Left.Left_Press");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            PlayerArrows.Play("Left.Left_None");
+        }
+
+        //Right Arrow Animation
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            PlayerArrows.Play("Down.Down_Press");
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            PlayerArrows.Play("Down.Down_None");
+        }
+
+        //Up Arrow Animation
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            PlayerArrows.Play("Up.Up_Press");
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            PlayerArrows.Play("Up.Up_None");
+        }
+
+        //Right Arrow Animation
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            PlayerArrows.Play("Right.Right_Press");
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            PlayerArrows.Play("Right.Right_None");
+        }
+    }
+}
