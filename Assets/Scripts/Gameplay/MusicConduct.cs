0ae7a031c26091de67a58b0df18574970055b2cd
Vanderley5974X <jvictorjohnv@gmail.com>
2021-06-24 04:23:47 -0300

    Added a ton of stuff + week1 test scene

---
+++ Assets/Scripts/Gameplay/MusicConduct.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicConduct : MonoBehaviour
+{
+    [Header("Technical Info")]
+    public float songBpm;
+    public float secPerBeat;
+    public float songPosition;
+    public float songPositionInBeats;
+    float dspSongTime;
+    [SerializeField] private float firstBeatOffset;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource voiceSource;
+    public GameObject Note;
+    public Slider Healthbar;
+    public Image BF_Canvas;
+    public Sprite[] Boyfriend;
+
+    [Header("Song Info")]
+    public float bpm;
+    public float[] notes;
+    public int nextIndex = 0;
+
+    //The animator controller to the player
+    public Animator Player;
+
+    void Start()
+    {
+        //Number of seconds in each beat
+        secPerBeat = 60f / songBpm;
+        bpm = songBpm;
+
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+    }
+    
+    void Update()
+    {
+        //Change UI character images based on health value
+        if(Healthbar.value > -25)
+        {
+            BF_Canvas.GetComponent<Image>().sprite = Boyfriend [1];
+        }
+
+        else
+        {
+            BF_Canvas.GetComponent<Image>().sprite = Boyfriend[0];
+        }
+
+        //determine how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+        //Sync voices & music
+        if (musicSource.isPlaying && voiceSource.isPlaying)
+        {
+            voiceSource.timeSamples = musicSource.timeSamples;
+        }
+
+        //determine how many beats since the song started
+        songPositionInBeats = songPosition / secPerBeat;
+        if (nextIndex < notes.Length && notes[nextIndex] < songPositionInBeats)
+        {
+            Instantiate(Note);
+            //initialize the fields of the music note
+            nextIndex++;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/SyncAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncAnimation : MonoBehaviour
+{
+ 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
