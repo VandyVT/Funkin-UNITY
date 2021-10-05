baed398cb7e05609fa55c96c24b3344e8f4610b2
wafflethings <60797216+wafflethings@users.noreply.github.com>
2021-10-05 21:44:55 +0100

    note anims mostly
    idfk why the dad animcontroller detaches when i start, when i fix that itll be done tho

--- Assets/Scripts/Gameplay/NoteInputManager.cs
+++ Assets/Scripts/Gameplay/NoteInputManager.cs
@@ -10,14 +10,27 @@ public class NoteInputManager : MonoBehaviour
     private StrumsController strums;
     public Character dad;
     public Character bf;
+    Animator bfAnim;
+    Animator dadAnim;
+
     private void Start()
     {
         strums = gameObject.GetComponent<StrumsController>();
         playerHolds = new bool[]{ false, false, false, false };
         enemyHolds = new bool[]{ false, false, false, false };
+        bfAnim = bf.GetComponent<Animator>();
+        dadAnim = dad.GetComponent<Animator>();
     }
     void Update()
     {
+        if (bfAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            bfAnim.Play("BF idle dance");
+        }
+        if (dadAnim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            dadAnim.Play("Dad idle dance");
+        }
         bool[] laneExitArray = new bool[] { false, false, false, false };
         if (!MusicConduct.Instance.started) return;
         for (int i = 0; i < allNotes.Count; i++)
@@ -31,6 +44,22 @@ public class NoteInputManager : MonoBehaviour
                     control.AtStrum = false;
                     control.gameObject.SetActive(false);
                     StartCoroutine(SwitchOffEnemyStrums(control.ArrowTypeSelect));
+                    if (control.ArrowTypeSelect == 0)
+                    {
+                        dadAnim.Play("Dad Sing Note LEFT", 0);
+                    }
+                    if (control.ArrowTypeSelect == 1)
+                    {
+                        dadAnim.Play("Dad Sing Note DOWN", 0);
+                    }
+                    if (control.ArrowTypeSelect == 2)
+                    {
+                        dadAnim.Play("Dad Sing Note UP", 0);
+                    }
+                    if (control.ArrowTypeSelect == 3)
+                    {
+                        dadAnim.Play("Dad Sing Note RIGHT", 0);
+                    }
                 }
                 else
                 {
@@ -40,6 +69,24 @@ public class NoteInputManager : MonoBehaviour
                         control.AtStrum = false;
                         control.gameObject.SetActive(false);
                         laneExitArray[control.ArrowTypeSelect] = true;
+
+                        
+                        if (control.ArrowTypeSelect == 0)
+                        {
+                            bfAnim.Play("BF NOTE LEFT", 0);
+                        }
+                        if (control.ArrowTypeSelect == 1)
+                        {
+                            bfAnim.Play("BF NOTE DOWN", 0);
+                        }
+                        if (control.ArrowTypeSelect == 2)
+                        {
+                            bfAnim.Play("BF NOTE UP", 0);
+                        }
+                        if (control.ArrowTypeSelect == 3)
+                        {
+                            bfAnim.Play("BF NOTE RIGHT", 0);
+                        }
                     }
                     if (!InputManager.HoldArray[control.ArrowTypeSelect])
                     {
